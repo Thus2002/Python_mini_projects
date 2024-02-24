6f88e6e17350a6a0445dd018ffcceb18a8ed6a27
Thushara J <138208974+Thus2002@users.noreply.github.com>
2024-02-24 20:52:42 +0530

    Create main.py

---
+++ Madlibs/main.py
@@ -0,0 +1,8 @@
+def mad_libs():
+    adjective = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adverb = input("Enter an adverb: ")
+    story = f"Once upon a time, there was a {adjective} {noun} who loved to {verb} {adverb}."
+    print(story)
+mad_libs()
